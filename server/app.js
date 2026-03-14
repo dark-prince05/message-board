b608f8fca2895f9480605be036a9f19dae59aa6e
dark-prince05 <barathraj0504@gmail.com>
2026-03-14 18:45:26 +0530

    update cors

--- server/app.js
+++ server/app.js
@@ -3,17 +3,29 @@ require("dotenv").config();
 const cors = require("cors");
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
-const pgPool = require('./db/pool') 
+const pgPool = require("./db/pool");
 const loginRouter = require("./routers/loginRouter");
 const msgRouter = require("./routers/msgRouter");
 const logoutRouter = require("./routers/logoutRouter");
 const membershipRouter = require("./routers/membershipRouter");
 
 const app = express();
+
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://darkprince-msg-board.netlify.app"
+]
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // allow
+      } else {
+        callback(new Error("Not allowed by CORS")); // block
+      }
+    },
     credentials: true
   }),
 );
@@ -22,8 +34,8 @@ app.use(
   session({
     store: new pgSession({
       pool: pgPool,
-      tableName: 'session',
-      createTableIfMissing: true
+      tableName: "session",
+      createTableIfMissing: true,
     }),
     secret: process.env.SECRET,
     resave: false,
@@ -31,7 +43,7 @@ app.use(
     cookie: {
       maxAge: 3 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === "development" ? 'lax' : 'none',
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
       secure: process.env.NODE_ENV !== "development",
     },
   }),
